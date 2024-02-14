541feddfc58ef019176162fab0cbccd3e3fe68dd
muhammadali057 <wygqkjsaq>
2024-02-14 09:20:21 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,191 @@
+
+
+const cars1El = document.querySelector(".cars1")
+const textEl = document.querySelector(".text")
+const texttitleEl = document.querySelector(".text_title")
+const dollorEl = document.querySelector(".dollor")
+const btn1El = document.querySelector(".btn1")
+
+
+const cars2El = document.querySelector(".cars2")
+const text1El = document.querySelector(".text1")
+const texttitle1El = document.querySelector(".text_title1")
+const dollor1El = document.querySelector(".dollor1")
+const btn2El = document.querySelector(".btn2")
+
+
+
+const cars3El = document.querySelector(".cars3")
+const text2El = document.querySelector(".text2")
+const texttitle2El = document.querySelector(".text_title2")
+const dollor2El = document.querySelector(".dollor2")
+const btn3El = document.querySelector(".btn3")
+
+
+
+const cars4El = document.querySelector(".cars4")
+const text3El = document.querySelector(".text3")
+const texttitle3El = document.querySelector(".text_title3")
+const dollor3El = document.querySelector(".dollor3")
+const btn4El = document.querySelector(".btn4")
+
+
+
+const cars5El = document.querySelector(".cars5")
+const text4El = document.querySelector(".text4")
+const texttitle4El = document.querySelector(".text_title4")
+const dollor4El = document.querySelector(".dollor4")
+const btn5El = document.querySelector(".btn5")
+
+
+
+const cars6El = document.querySelector(".cars6")
+const text5El = document.querySelector(".text5")
+const texttitle5El = document.querySelector(".text_title5")
+const dollor5El = document.querySelector(".dollor5")
+const btn6El = document.querySelector(".btn6")
+
+
+
+const cars7El = document.querySelector(".cars7")
+const text6El = document.querySelector(".text6")
+const texttitle6El = document.querySelector(".text_title6")
+const dollor6El = document.querySelector(".dollor6")
+const btn7El = document.querySelector(".btn7")
+
+
+
+const cars8El = document.querySelector(".cars8")
+const text7El = document.querySelector(".text7")
+const texttitle7El = document.querySelector(".text_title7")
+const dollor7El = document.querySelector(".dollor7")
+const btn8El = document.querySelector(".btn8")
+
+
+
+const cars9El = document.querySelector(".cars9")
+const text8El = document.querySelector(".text8")
+const texttitle8El = document.querySelector(".text_title8")
+const dollor8El = document.querySelector(".dollor8")
+const btn9El = document.querySelector(".btn9")
+
+
+
+const cars10El = document.querySelector(".cars10")
+const text9El = document.querySelector(".text9")
+const texttitle9El = document.querySelector(".text_title9")
+const dollor9El = document.querySelector(".dollor9")
+const btn10El = document.querySelector(".btn10")
+
+
+
+
+const cars11El = document.querySelector(".cars11")
+const text10El = document.querySelector(".text10")
+const texttitle10El = document.querySelector(".text_title10")
+const dollor10El = document.querySelector(".dollor10")
+const btn11El = document.querySelector(".btn11")
+
+
+
+const cars12El = document.querySelector(".cars12")
+const text11El = document.querySelector(".text11")
+const texttitle11El = document.querySelector(".text_title11")
+const dollor11El = document.querySelector(".dollor11")
+const btn12El = document.querySelector(".btn12")
+
+btn1El.addEventListener('click', () => {
+    cars1El.style.backgroundColor = "gray"
+    textEl.style.color = "gold"
+    texttitleEl.style.color = "gold"
+    btn1El.style.backgroundColor = "blue"
+    dollorEl.style.color = "gold"
+})
+
+btn2El.addEventListener('click', () => {
+    cars2El.style.backgroundColor = "gray"
+    text1El.style.color = "gold"
+    texttitle1El.style.color = "gold"
+    btn2El.style.backgroundColor = "blue"
+    dollor1El.style.color = "gold"
+})
+
+btn3El.addEventListener('click', () => {
+    cars3El.style.backgroundColor = "gray"
+    text2El.style.color = "gold"
+    texttitle2El.style.color = "gold"
+    btn3El.style.backgroundColor = "blue"
+    dollor2El.style.color = "gold"
+})
+
+btn4El.addEventListener('click', () => {
+    cars4El.style.backgroundColor = "gray"
+    text3El.style.color = "gold"
+    texttitle3El.style.color = "gold"
+    btn4El.style.backgroundColor = "blue"
+    dollor3El.style.color = "gold"
+})
+
+btn5El.addEventListener('click', () => {
+    cars5El.style.backgroundColor = "gray"
+    text4El.style.color = "gold"
+    texttitle4El.style.color = "gold"
+    btn5El.style.backgroundColor = "blue"
+    dollor4El.style.color = "gold"
+})
+
+btn6El.addEventListener('click', () => {
+    cars6El.style.backgroundColor = "gray"
+    text5El.style.color = "gold"
+    texttitle5El.style.color = "gold"
+    btn6El.style.backgroundColor = "blue"
+    dollor5El.style.color = "gold"
+})
+
+btn7El.addEventListener('click', () => {
+    cars7El.style.backgroundColor = "gray"
+    text6El.style.color = "gold"
+    texttitle6El.style.color = "gold"
+    btn7El.style.backgroundColor = "blue"
+    dollor6El.style.color = "gold"
+})
+
+btn8El.addEventListener('click', () => {
+    cars8El.style.backgroundColor = "gray"
+    text7El.style.color = "gold"
+    texttitle7El.style.color = "gold"
+    btn8El.style.backgroundColor = "blue"
+    dollor7El.style.color = "gold"
+})
+
+btn9El.addEventListener('click', () => {
+    cars9El.style.backgroundColor = "gray"
+    text8El.style.color = "gold"
+    texttitle8El.style.color = "gold"
+    btn9El.style.backgroundColor = "blue"
+    dollor8El.style.color = "gold"
+})
+
+btn10El.addEventListener('click', () => {
+    cars10El.style.backgroundColor = "gray"
+    text9El.style.color = "gold"
+    texttitle9El.style.color = "gold"
+    btn10El.style.backgroundColor = "blue"
+    dollor9El.style.color = "gold"
+})
+
+btn11El.addEventListener('click', () => {
+    cars11El.style.backgroundColor = "gray"
+    text10El.style.color = "gold"
+    texttitle10El.style.color = "gold"
+    btn11El.style.backgroundColor = "blue"
+    dollor10El.style.color = "gold"
+})
+
+btn12El.addEventListener('click', () => {
+    cars12El.style.backgroundColor = "gray"
+    text11El.style.color = "gold"
+    texttitle11El.style.color = "gold"
+    btn12El.style.backgroundColor = "blue"
+    dollor11El.style.color = "gold"
+})
